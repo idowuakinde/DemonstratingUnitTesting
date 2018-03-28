77b879449d41aec021f0b9eab225baf110db842c
idowuakinde <8018104+idowuakinde@users.noreply.github.com>
2018-03-28 13:39:21 +0100

    Initial check-in of module DemonstratingUnitTesting

---
+++ DemonstratingUnitTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DemonstratingUnitTesting
+{
+    public class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public double Add(double firstOperand, double secondOperand)
+        {
+            return (firstOperand + secondOperand);
+        }
+
+        public double Subtract(double firstOperand, double secondOperand)
+        {
+            return (firstOperand - secondOperand);
+        }
+
+        public double Divide(double firstOperand, double secondOperand)
+        {
+            return (firstOperand / secondOperand);
+        }
+
+        public double Multiply(double firstOperand, double secondOperand)
+        {
+            return (firstOperand * secondOperand);
+        }
+    }
+}
---
+++ DemonstratingUnitTestingTests/Test.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using DemonstratingUnitTesting;
+
+namespace DemonstratingUnitTestingTests
+{
+    [TestFixture()]
+    public class CalculatorTest
+    {
+        [Test()]
+        public void TestAddition()
+        {
+            // arrange
+            Calculator calculator = new Calculator();
+            double X = 15;
+            double Y = 25;
+            double result = 0;
+
+            // act
+            result = calculator.Add(X, Y);
+
+            // assert
+            Assert.AreEqual(40, result);
+        }
+
+        [Test()]
+        public void TestSubtraction()
+        {
+            // arrange
+            Calculator calculator = new Calculator();
+            double X = 50;
+            double Y = 25;
+            double result = 0;
+
+            // act
+            result = calculator.Subtract(X, Y);
+
+            // assert
+            Assert.AreEqual(25, result);
+        }
+
+        [Test()]
+        public void TestMultiplication()
+        {
+            // arrange
+            Calculator calculator = new Calculator();
+            double X = 5;
+            double Y = 20;
+            double result = 0;
+
+            // act
+            result = calculator.Multiply(X, Y);
+
+            // assert
+            Assert.AreEqual(100, result);
+        }
+
+        [Test()]
+        public void TestDivision()
+        {
+            // arrange
+            Calculator calculator = new Calculator();
+            double X = 50;
+            double Y = 25;
+            double result = 0;
+
+            // act
+            result = calculator.Divide(X, Y);
+
+            // assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
